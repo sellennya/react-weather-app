bee881840a907091b287947b062e005aa298203a
sellennya <57621268+sellennya@users.noreply.github.com>
2022-09-07 13:03:13 +0100

    add the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,19 @@ import React from 'react';
 import './App.css';
 
 export default function App() {
-  return <div className='App'>Weather App</div>;
+  return (
+    <div className='App'>
+      <h1> Weather App</h1>
+      <footer>
+        This project was coded by Sandra Cardoso and is {''}
+        <a
+          href='https://github.com/sellennya/react-weather-app'
+          target='_blank'
+          rel=' noreferrer'
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }

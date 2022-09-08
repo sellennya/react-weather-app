5b9322c5cd4d60b5b33e7e9216d3bfcc39d9dc40
sellennya <57621268+sellennya@users.noreply.github.com>
2022-09-08 09:00:56 +0100

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import './App.css';
 export default function App() {
   return (
     <div className='App'>
-      <h1> Weather App</h1>
-      <footer>
-        This project was coded by Sandra Cardoso and is {''}
-        <a
-          href='https://github.com/sellennya/react-weather-app'
-          target='_blank'
-          rel=' noreferrer'
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className='container'>
+        <h1> Weather App</h1>
+        <footer>
+          This project was coded by Sandra Cardoso and is {''}
+          <a
+            href='https://github.com/sellennya/react-weather-app'
+            target='_blank'
+            rel=' noreferrer'
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
